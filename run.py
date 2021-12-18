bc03bf2231dbf005398dbfab52b8477df7c02097
Eduardo <egbulsoni@gmail.com>
2021-12-18 14:37:25 -0300

    aula 4

---
+++ run.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__) #nuevo objeto,
+#http://localhost:8000/params?params1=Eduardo_Bulsoni&params2=test_dos
+#http://localhost:8000/params
+
+@app.route('/') #wrap o un decorador
+def index():
+    return "Hola Mundo" #Regresar un string
+
+@app.route('/saluda') #wrap o un decorador
+def saluda():
+    return "Otro mensaje" #Regresar un string
+
+# /params?ahdkahskd=alsdkflkw
+
+@app.route('/params') #wrap o un decorador
+def params():
+    param = request.args.get('params1', 'no contiene este parametro.')
+    param_dos = request.args.get('params2', 'no contiene este parametro.')
+    return "El parametro es {} , {}".format(param, param_dos) #Regresar un string
+
+app.run( debug = True, port= 8000) #Se encarga de ejecutar el servidor 5000
